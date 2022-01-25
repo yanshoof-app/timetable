8e165782ee463e19dcdc76b3e19380255c604e90
itays123 <itays2005@gmail.com>
2022-01-25 13:59:11 +0200

    INSTALL: Tailwind shit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,8 +2,6 @@ import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 
-import '../styles/globals.css';
-
 type Props = {
   children?: ReactNode;
   title?: string;
