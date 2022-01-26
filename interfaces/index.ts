a04f75e81ac98069bf3c75281bec2448a601a5c4
itays123 <itays2005@gmail.com>
2022-01-26 13:10:39 +0200

    FEATURE: Changes

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,4 +1,21 @@
-export interface ILesson {
+import { HourOfDay } from '../utils/data/iScoolTypes';
+
+export enum LessonModification {
+  None = 0,
+  Canceled,
+  Exam,
+  NewTeacher,
+  NewRoom,
+  NewHour,
+  Other,
+}
+
+export interface Modification {
+  modification?: LessonModification;
+  modData?: string | number; // modification data if needed
+}
+
+export interface ILesson extends Modification {
   subject: string;
   teacher: string;
   class: string; // Room string / Zoom / Async
--- utils/data/iScoolTypes.ts
+++ utils/data/iScoolTypes.ts
@@ -1,12 +1,15 @@
-import { ILesson } from '../../interfaces';
+import { ILesson, LessonModification, Modification } from '../../interfaces';
 import { CLASS_UNAVAILABLE, ONLINE, ONLINE_ASYNCRONOUS } from '../strings';
 
 export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 export type HourOfDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-export interface ILessonIscool {
+export interface IStudyGroupIscool {
   Teacher: string;
   Subject: string;
+}
+
+export interface ILessonIscool extends IStudyGroupIscool {
   Room: string;
   Note: string;
   Te: string; // zoom or asyncronous
@@ -16,6 +19,24 @@ export interface ILessonArrMemberIscool {
   Day: DayOfWeek;
   Hour: HourOfDay;
   Lessons: ILessonArrMemberIscool[];
+}
+
+type IscoolChangeType =
+  | 'FreeLesson'
+  | 'Exam'
+  | 'NewTeacher'
+  | 'NewRoom'
+  | 'NewHour';
+
+export interface IChangeIscool {
+  Date: Date;
+  Hour: HourOfDay;
+  ChangeType: IscoolChangeType;
+  FixType: string;
+  StudyGroup: IStudyGroupIscool;
+  NewRoom: string;
+  NewTeacher: string;
+  NewHour: HourOfDay;
 }
 
 export namespace ISCOOL {
@@ -28,11 +49,54 @@ export namespace ISCOOL {
       ? ONLINE_ASYNCRONOUS
       : CLASS_UNAVAILABLE;
   }
-  function toLesson({ Subject, Teacher, Te, Room }: ILessonIscool): ILesson {
+  function toChange(
+    Subject: string,
+    Teacher: String,
+    change?: IChangeIscool
+  ): Modification {
+    if (
+      !change ||
+      change.StudyGroup.Subject != Subject ||
+      change.StudyGroup.Teacher != Teacher
+    )
+      return {};
+
+    switch (change.ChangeType) {
+      case 'FreeLesson':
+        return { modification: LessonModification.Canceled };
+      case 'Exam':
+        return { modification: LessonModification.Exam };
+      case 'NewTeacher':
+        return {
+          modification: LessonModification.NewTeacher,
+          modData: change.NewTeacher,
+        };
+      case 'NewRoom':
+        return {
+          modification: LessonModification.NewRoom,
+          modData: change.NewRoom,
+        };
+      case 'NewHour':
+        return {
+          modification: LessonModification.NewHour,
+          modData: change.NewHour,
+        };
+      default:
+        return {
+          modification: LessonModification.Other,
+          modData: change.ChangeType + change.FixType,
+        };
+    }
+  }
+  function toLesson(
+    { Subject, Teacher, Te, Room }: ILessonIscool,
+    change?: IChangeIscool
+  ): ILesson {
     return {
       subject: Subject,
       teacher: Teacher,
       class: toClass(Te, Room),
+      ...toChange(Subject, Teacher, change),
     };
   }
 }
