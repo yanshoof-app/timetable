bfa18d5f8362a6404972f511f83daa13824a13db
itays123 <itays2005@gmail.com>
2022-01-26 11:44:36 +0200

    REFACTOR: Cleanup project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <h1 className="text-2xl text-center">Hello Next.js 👋</h1>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
