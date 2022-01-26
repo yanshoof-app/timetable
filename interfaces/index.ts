2bf62d11336a4ee74124629d1d81c6fa9ec3a9d2
itays123 <itays2005@gmail.com>
2022-01-26 12:13:24 +0200

    REAFCTOR: Move types to iscool types

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -4,9 +4,6 @@ export interface ILesson {
   class: string; // Room string / Zoom / Async
 }
 
-export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-export type HourOfDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-
 export type DailySchedule = ILesson[];
 export type WeeklySchedule = DailySchedule[7];
 
--- utils/data/iScoolTypes.ts
+++ utils/data/iScoolTypes.ts
@@ -1,5 +1,8 @@
-import { DayOfWeek, HourOfDay, ILesson } from '../../interfaces';
+import { ILesson } from '../../interfaces';
 import { CLASS_UNAVAILABLE, ONLINE, ONLINE_ASYNCRONOUS } from '../strings';
+
+export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+export type HourOfDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export interface ILessonIscool {
   Teacher: string;
