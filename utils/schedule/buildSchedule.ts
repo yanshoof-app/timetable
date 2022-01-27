a6df3f832acb0dd26eed9edc35c17ba93919d6e8
itays123 <itays2005@gmail.com>
2022-01-27 08:06:38 +0200

    REFACTOR: Use constants

--- utils/schedule/buildSchedule.ts
+++ utils/schedule/buildSchedule.ts
@@ -1,12 +1,17 @@
 import { LessonOrMultiple } from '../../interfaces';
 import { ISCOOL } from '../iScool';
-import { ILessonArrMemberIscool } from './types';
+import {
+  DAYS_IN_WEEK,
+  HOURS_OF_SCHEDULE,
+  ILessonArrMemberIscool,
+} from './types';
 
 export default function buildFullSchedule(
   schedule: ILessonArrMemberIscool[]
 ): LessonOrMultiple[][] {
-  let result: LessonOrMultiple[][] = new Array(7);
-  for (let i = 0; i < 7; i++) result[i] = new Array(12);
+  let result: LessonOrMultiple[][] = new Array(DAYS_IN_WEEK);
+  for (let i = 0; i < DAYS_IN_WEEK; i++)
+    result[i] = new Array(HOURS_OF_SCHEDULE);
   schedule.forEach(lesson => {
     const day = lesson.Day;
     const hourIndex = lesson.Hour - 1;
--- utils/schedule/types.ts
+++ utils/schedule/types.ts
@@ -1,3 +1,6 @@
+export const DAYS_IN_WEEK = 7;
+export const HOURS_OF_SCHEDULE = 12; // change if needed
+
 export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 export type HourOfDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
