978da30d6abc574d742dbb9df488cc86e2f78827
itays123 <itays2005@gmail.com>
2022-02-25 18:54:49 +0200

    FEATURE: SettingsBox

---
+++ components/settings/SettingsBox.tsx
@@ -0,0 +1,34 @@
+import { ForwardRTL } from '../icons'
+import { ThemeColor } from '../theme'
+import ShadowedWrapper from '../ui/ShadowedWrapper'
+
+export interface SettingsBoxProps {
+  color: ThemeColor
+  label: string
+  value: string
+  onClick?(): unknown
+  className?: string
+}
+
+export default function SettingsBox({
+  color,
+  label,
+  value,
+  onClick = () => {},
+  className = '',
+}: SettingsBoxProps) {
+  return (
+    <ShadowedWrapper
+      color={color}
+      className={`relative rounded-[12px] px-2 py-4 ${className}`}
+    >
+      <div className="inset-0 flex flex-col space-y-1" onClick={onClick}>
+        <p className="font-semibold">{label}</p>
+        <div className="flex justify-end items-center text-gray-600">
+          <span>{value}</span>
+          <ForwardRTL width={14} height={14} />
+        </div>
+      </div>
+    </ShadowedWrapper>
+  )
+}
---
+++ pages/settings.tsx
@@ -0,0 +1,20 @@
+import Layout from '../components/Layout'
+import SettingsBox from '../components/settings/SettingsBox'
+import Navbar from '../components/ui/Navbar'
+
+export default function Settings() {
+  return (
+    <Layout title="Settings">
+      <div className="flex p-8">
+        <SettingsBox
+          color="primary"
+          label="כיתה ובית ספר"
+          value="י7, עמי אסף בית ברל"
+          className="basis-1/2"
+        />
+      </div>
+
+      <Navbar />
+    </Layout>
+  )
+}
