5576776daa8d73b75eab8a6ac85e6c57c12f23c9
itays123 <itays2005@gmail.com>
2022-02-26 22:49:15 +0200

    FEATURE: StorageProvider component (not ready)

---
+++ contexts/Storage/wrappers.ts
@@ -0,0 +1,23 @@
+import { createLogicalWrapper } from '../utils'
+import { StorageContext } from '.'
+
+export const SchoolExists = createLogicalWrapper(
+  StorageContext,
+  ({ school }) => !!school
+)
+export const ClassExists = createLogicalWrapper(
+  StorageContext,
+  ({ classId }) => !!classId
+)
+export const ThemePreferenceExists = createLogicalWrapper(
+  StorageContext,
+  ({ theme }) => !!theme
+)
+export const UpdateTimeExists = createLogicalWrapper(
+  StorageContext,
+  ({ updateTime }) => !!updateTime
+)
+export const OthersChangesPreferenceExists = createLogicalWrapper(
+  StorageContext,
+  ({ scheduleSettings }) => !!scheduleSettings.showOthersChanges
+)
