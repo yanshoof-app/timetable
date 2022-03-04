f438dd6dcb3a2b439a417b0ba2ecd083e8141a34
Itay Oshri <itay.oshri@gmail.com>
2022-03-04 19:51:55 +0200

    FIX: OthersChangesPick with useStorage

--- components/settings/OthersChangesPick.tsx
+++ components/settings/OthersChangesPick.tsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
+import { useStorage } from '../../contexts/Storage'
 import RadioButton from '../forms/RadioButton'
 import OthersChangesExample from './examples/OthersChangesExample'
 
-export default function OthersChangesPick(OthersChanges: boolean) {
-  const [toShow, setShow] = useState(OthersChanges)
+export default function OthersChangesPick() {
+  const { showOthersChanges, setOthersChangesPreference } = useStorage()
 
   return (
     <div className="p-[1rem] flex flex-col gap-2">
       <div className="bg-uiPrimary-200 flex flex-row justify-between p-3 rounded-[26.4px] gap-3 select-none fk">
         <OthersChangesExample
-          onClick={() => setShow(false)}
+          onClick={() => setOthersChangesPreference(false)}
           variant={'hide'}
           className="h-fit"
         />
         <OthersChangesExample
-          onClick={() => setShow(true)}
+          onClick={() => setOthersChangesPreference(true)}
           variant={'show'}
           className=""
         />
       </div>
       <div className="flex justify-between px-[calc(25%-12px)] h-fit">
         <RadioButton
-          selected={!toShow}
+          selected={!showOthersChanges}
           label={'הסתר'}
-          onClick={() => setShow(false)}
+          onClick={() => setOthersChangesPreference(false)}
         />
         <RadioButton
-          selected={toShow}
+          selected={showOthersChanges}
           label={'הצג'}
-          onClick={() => setShow(true)}
+          onClick={() => setOthersChangesPreference(true)}
         />
       </div>
     </div>
