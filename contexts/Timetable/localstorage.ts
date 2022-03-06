6bcc5897c932a766f4288e9e3bb0e88cc4a5478c
itays123 <itays2005@gmail.com>
2022-03-06 17:23:29 +0200

    FIX: Delete saved timetable on week start

--- contexts/Timetable/localstorage.ts
+++ contexts/Timetable/localstorage.ts
@@ -1,11 +1,14 @@
 import { createLocalStorageState } from '../../hooks/useLocalStorageState'
 import { ILesson } from '../../interfaces'
 
+const isNewWeek = new Date().getDay() == 0
+
 export const useLessonMatrixState = createLocalStorageState<ILesson[][]>(
   'lessons',
   {
     decode: (str?: string) => {
       try {
+        if (isNewWeek) return []
         return (JSON.parse(str) as ILesson[][] | null) || []
       } catch (e) {
         return []
