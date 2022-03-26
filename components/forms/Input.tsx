02b739e3a8bf4c6eb7b405c1da0ee45f21a5dc5f
Itay Oshri <itay.oshri@gmail.com>
2022-03-26 19:28:05 +0300

    REFACTOR: Disable spell-check

--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -38,6 +38,7 @@ export default function Input({
         placeholder={selected ? '' : hint}
         onSelect={() => setSelected(true)}
         onBlur={() => setSelected(false)}
+        spellCheck={false}
       ></input>
     </div>
   )
