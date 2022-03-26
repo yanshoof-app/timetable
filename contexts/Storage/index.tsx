f2d029661d8ffa9b3b1dac838aa0819bc0845db4
Itay Oshri <itay.oshri@gmail.com>
2022-03-26 12:41:57 +0300

    FIX: Full loading screen instead of timetable only

--- contexts/Storage/index.tsx
+++ contexts/Storage/index.tsx
@@ -1,8 +1,8 @@
 // the context responsible for handling storage
 
-import { createContext, useCallback } from 'react'
-import TimetableSkeleton from '../../components/timetable/TimetableSkeleton'
+import { createContext } from 'react'
 import { Wrapper } from '../../components/types'
+import LoadingScreen from '../../components/ui/LoadingScreen'
 import { useClientRender } from '../../hooks/useClientRender'
 import { createUseContextHook } from '../utils'
 import {
@@ -34,7 +34,7 @@ export default function StorageProvider({ children }: Wrapper) {
   const [lastUserUpdate, setLastUserUpdate] = useLastUserUpdate()
   const isClient = useClientRender()
 
-  if (!isClient) return <TimetableSkeleton />
+  if (!isClient) return <LoadingScreen />
 
   return (
     <StorageContext.Provider
