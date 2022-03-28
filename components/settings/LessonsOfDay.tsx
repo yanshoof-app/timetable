d1401c91e3c2f59b75192c8a059481d5bae4444b
Itay Oshri <itay.oshri@gmail.com>
2022-03-28 20:49:50 +0300

    FEATURE: Display lessons skeletons while loading

--- components/settings/LessonsOfDay.tsx
+++ components/settings/LessonsOfDay.tsx
@@ -1,6 +1,7 @@
 import { HEBREW_DAYS } from '../../hooks/useHebrewDate'
 import useRanges from '../../hooks/useRanges'
 import { DayOfWeek, HourOfDay, LessonOrMultiple } from '../../interfaces'
+import { SkeletonLesson } from '../timetable/Lesson/Skeleton'
 import LessonPick from '../timetable/LessonPick'
 
 export interface LessonsOfDayProps {
@@ -15,21 +16,23 @@ export default function LessonsOfDay({
   timetable,
 }: LessonsOfDayProps) {
   const rangedHours = useRanges(hourSet)
-  return timetable[day] ? (
+  return (
     <div className="flex flex-col gap-2 py-2">
       <p className="font-semibold text-lg px-5">{`יום ${HEBREW_DAYS[day]}`}</p>
       <div className="flex flex-col gap-4">
-        {rangedHours.map((hours, index) => (
-          <LessonPick
-            day={day}
-            hour={hours as HourOfDay[]}
-            key={index}
-            editable
-          ></LessonPick>
-        ))}
+        {rangedHours.map((hours, index) =>
+          timetable[day] ? (
+            <LessonPick
+              day={day}
+              hour={hours as HourOfDay[]}
+              key={index}
+              editable
+            ></LessonPick>
+          ) : (
+            <SkeletonLesson key={index} />
+          )
+        )}
       </div>
     </div>
-  ) : (
-    <div></div>
   )
 }
