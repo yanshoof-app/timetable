5bbd0e33fd0397436453cb4ffb5e8e93626ca4fe
Itay Oshri <itay.oshri@gmail.com>
2022-04-04 15:27:38 +0300

    FIX: Navbar center

--- components/ui/Navbar/index.tsx
+++ components/ui/Navbar/index.tsx
@@ -6,7 +6,7 @@ const BY_TEACHER = 'לפי מורה'
 const SETTINGS = 'הגדרות'
 
 const Navbar = () => (
-  <nav className="flex justify-between items-center h-14 px-10 bg-white fixed bottom-0 inset-x-0 z-10 border-gray-200 border-t-[1px]">
+  <nav className="grid grid-cols-3 items-center w-full h-14 bg-white fixed bottom-0 inset-x-0 z-10 border-gray-200 border-t-[1px]">
     <NavLink to="/" label={MY_SCHEDULE} icon={Calendar} />
     <NavLink to="/teachers" label={BY_TEACHER} icon={LightBulb} />
     <NavLink to="/settings" label={SETTINGS} icon={Settings} />
