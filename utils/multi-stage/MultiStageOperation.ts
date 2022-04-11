a2c51fb991bd9c1aeb3bfd2bc8e6191d0971405a
itays123 <itays2005@gmail.com>
2022-04-11 22:38:01 +0300

    FEATURE: MultiClassRequest

---
+++ utils/multi-stage/MultiStageOperation.ts
@@ -0,0 +1,35 @@
+import { ListenerSignature, TypedEmitter } from 'tiny-typed-emitter'
+
+/**
+ * Represents events that should be emitted by any multiple stage operation
+ */
+export interface MultiStageOperationEvents<Success, Error> {
+  ready: (result: Success) => void
+  error: (err: Error) => void
+  delay: () => void
+}
+
+type TypeSafeMSOE<Success, Error, T> = Omit<T, 'error' | 'ready' | 'delay'> &
+  MultiStageOperationEvents<Success, Error>
+
+/**
+ * Represents an operation that takes a long time to process
+ * @abstract
+ * @extends TypedEmitter for the ability to emit and subscribe to events
+ * @author Itay Schechner
+ * @version 2022.0.1
+ */
+abstract class MultiStageOperation<
+  Success,
+  Error,
+  T extends ListenerSignature<T> = ListenerSignature<unknown>
+> extends TypedEmitter<TypeSafeMSOE<Success, Error, T>> {
+  protected emitError(err: Error) {
+    const params = [err] as Parameters<TypeSafeMSOE<Success, Error, T>['error']> // this must be [Error] type
+    this.emit('error', ...params)
+  }
+
+  public abstract begin(): Promise<void>
+}
+
+export default MultiStageOperation
