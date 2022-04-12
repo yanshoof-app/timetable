9f800e431fcdccaa544c54ef392b69180e62911d
itays123 <itays2005@gmail.com>
2022-04-12 14:30:08 +0300

    FEATURE: Others changes page

--- pages/settings/changes.tsx
+++ pages/settings/changes.tsx
@@ -1,7 +1,49 @@
-import OthersChangesPick from '../../components/settings/OthersChangesPick'
+import { useState } from 'react'
+import RadioButton from '../../components/forms/RadioButton'
+import OthersChangesExample from '../../components/settings/examples/OthersChangesExample'
+import SettingsPageLayout from '../../components/settings/SettingsPageLayout'
+import { useStorage } from '../../contexts/Storage'
 import useBackPress from '../../hooks/useBackPress'
 
 export default function OthersChangesSetting() {
   const { back } = useBackPress('/settings')
-  return <OthersChangesPick onBackPress={back} />
+  const { setOthersChangesPreference, showOthersChanges } = useStorage()
+  const [selectedPreference, setSelectedPreference] =
+    useState(showOthersChanges)
+  return (
+    <SettingsPageLayout
+      title="שינויים של אחרים"
+      onBackPress={() => {
+        setOthersChangesPreference(selectedPreference)
+        back()
+      }}
+    >
+      <div className="flex flex-col gap-2">
+        <div className="bg-uiPrimary-200 flex flex-row justify-between p-3 rounded-[26.4px] gap-3 select-none fk">
+          <OthersChangesExample
+            onClick={() => setSelectedPreference(false)}
+            variant={'hide'}
+            className="h-fit"
+          />
+          <OthersChangesExample
+            onClick={() => setSelectedPreference(true)}
+            variant={'show'}
+            className="h-[292.22px]" //TODO: Find a better method to make them same height
+          />
+        </div>
+        <div className="flex justify-between px-[calc(25%-12px)] h-fit">
+          <RadioButton
+            selected={!selectedPreference}
+            label={'הסתר'}
+            onClick={() => setSelectedPreference(false)}
+          />
+          <RadioButton
+            selected={selectedPreference}
+            label={'הצג'}
+            onClick={() => setSelectedPreference(true)}
+          />
+        </div>
+      </div>
+    </SettingsPageLayout>
+  )
 }
