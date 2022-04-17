3b73b2995cd38c4ec5e8dd9eaa05691d6e71fa4e
itays123 <itays2005@gmail.com>
2022-04-17 14:43:30 +0300

    FEATURE: Teacher Lookup query

--- utils/teacherList/TeacherList.ts
+++ utils/teacherList/TeacherList.ts
@@ -1,23 +1,39 @@
+import { TypedEmitter } from 'tiny-typed-emitter'
 import { ILesson, ILessonArrMemberIscool, ITimetable } from '../../interfaces'
 import { ITeacherList } from '../../interfaces/teacherList'
 
 /**
- * A class that its objects contains a teacher list
- * @author Itay Oshri
- * @version 2022.0.0
+ * Represents events of the TeacherList object
  */
-export class TeacherList implements ITeacherList {
+export interface ITeacherListEvents {
+  teacherAdded: (teacherName: string) => void
+}
+
+/**
+ * A class that its objects contains a teacher list
+ * @extends TypedEmitter<ITeacherList> to subscribe to teacher addition events
+ * @authors Itay Oshri, Itay Schechner
+ * @version 2022.0.1
+ */
+export class TeacherList
+  extends TypedEmitter<ITeacherListEvents>
+  implements ITeacherList
+{
   private teacherSet: Set<string>
 
   constructor() {
+    super()
     this.teacherSet = new Set()
   }
 
   public fromIscool(schedule: ILessonArrMemberIscool[]) {
     for (let lesson of schedule) {
-      lesson.Lessons.forEach((studyGroup) => {
-        if (studyGroup.Teacher !== '') {
-          this.teacherSet.add(studyGroup.Teacher)
+      lesson.Lessons.forEach(({ Teacher }) => {
+        if (Teacher !== '') {
+          if (!this.teacherSet.has(Teacher)) {
+            this.emit('teacherAdded', Teacher)
+            this.teacherSet.add(Teacher)
+          }
         }
       })
     }
