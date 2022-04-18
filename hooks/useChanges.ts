a8759275c72019bae245f7b8f87f85c6305824d4
Itay Oshri <itay.oshri@gmail.com>
2022-04-18 15:48:25 +0300

    REFACTOR: useChanges

--- hooks/useChanges.ts
+++ hooks/useChanges.ts
@@ -6,93 +6,71 @@ import {
   IModification,
   LessonModification,
 } from '../interfaces'
-import { initArray } from '../utils/data/arrays'
+import { initArray, initMatrix } from '../utils/data/arrays'
 
 export interface ChangeInfo {
-  day: DayOfWeek
-  hour: HourOfDay
-  studyGroup?: string
   typeOfChange: LessonModification
-  change?: string
+  data?: string
 }
 
-const infoToStudyGroup = (subject: string, teacher: string) => {
-  return `${teacher && `${teacher} עם`} ${subject}`
+export interface HourlyChanges {
+  hour: HourOfDay
+  studyGroup: string
+  changes: ChangeInfo[]
+  otherChanges: ChangeInfo[]
+  events: ChangeInfo[]
+}
+
+const infoToStudyGroup = (lesson: ILesson) => {
+  const { subject, teacher } = lesson
+  return teacher ? `${subject} עם ${teacher}` : subject
 }
 
 export const modToChange = (
-  day: DayOfWeek,
-  hour: HourOfDay,
   modification: IModification,
   lesson: ILesson
 ): ChangeInfo => {
-  const studyGroup = infoToStudyGroup(lesson.subject, lesson.teacher)
-
   switch (modification.modification) {
     case LessonModification.NewHour:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
-        change: `${lesson.subject} ל${modification.modData}`,
+        data: `${lesson.subject} ל${modification.modData}`,
       }
 
     case LessonModification.NewRoom:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
-        change: `${lesson.class}מ ${modification.modData}ל`,
+        data: `${lesson.class}מ ${modification.modData}ל`,
       }
 
     case LessonModification.NewTeacher:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
-        change: `${modification.modData} מחליף את ${lesson.teacher}`,
+        data: `${modification.modData} מחליף את ${lesson.teacher}`,
       }
 
     case LessonModification.Exam:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
-        change: `${modification.modData}ב`,
+        data: `${modification.modData}ב`,
       }
 
     case LessonModification.Canceled:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
       }
     default:
       return {
-        day: day,
-        hour: hour,
-        studyGroup: studyGroup,
         typeOfChange: modification.modification,
-        change: modification.modData as string,
+        data: modification.modData as string,
       }
   }
 }
 
-export const eventToChange = (
-  day: DayOfWeek,
-  hour: HourOfDay,
-  event: string
-): ChangeInfo => {
+export const eventToChange = (event: string): ChangeInfo => {
   return {
-    day: day,
-    hour: hour,
     typeOfChange: LessonModification.Other,
-    change: event,
+    data: event,
   }
 }
 
@@ -101,32 +79,39 @@ export const eventToChange = (
  */
 export default function useChanges(lessons: ILesson[][]) {
   return useMemo(() => {
-    const changes = initArray<ChangeInfo>(7)
+    const changes = initArray<HourlyChanges>(7)
     for (let day in lessons) {
       for (let hour in lessons[day]) {
         const lesson = lessons[day][hour]
         const dayOfWeek = Number(day) as DayOfWeek
         const hourOfDay = Number(hour) as HourOfDay
 
+        if (lesson.changes || lesson.otherChanges || lesson.events)
+          changes[dayOfWeek][hourOfDay] = {
+            hour: hourOfDay,
+            studyGroup: infoToStudyGroup(lesson),
+            changes: [],
+            otherChanges: [],
+            events: [],
+          }
+
         if (lesson.changes) {
           for (let change of lesson.changes) {
-            changes[dayOfWeek].push(
-              modToChange(dayOfWeek, hourOfDay, change, lesson)
-            )
+            changes[dayOfWeek][hour].changes.push(modToChange(change, lesson))
           }
         }
 
         if (lesson.otherChanges) {
           for (let change of lesson.otherChanges) {
-            changes[dayOfWeek].push(
-              modToChange(dayOfWeek, hourOfDay, change, lesson)
+            changes[dayOfWeek][hour].otherChanges.push(
+              modToChange(change, lesson)
             )
           }
         }
 
         if (lesson.events) {
           for (let event of lesson.events) {
-            changes[dayOfWeek].push(eventToChange(dayOfWeek, hourOfDay, event))
+            changes[dayOfWeek][hour].events.push(eventToChange(event))
           }
         }
       }
