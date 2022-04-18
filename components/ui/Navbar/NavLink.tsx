0b8e70fa0885c71386fde75381ced4663f172fd7
Itay Oshri <itay.oshri@gmail.com>
2022-04-18 19:28:37 +0300

    FIX: Show NavLink update only if > 0

--- components/ui/Navbar/NavLink.tsx
+++ components/ui/Navbar/NavLink.tsx
@@ -28,10 +28,8 @@ export default function NavLink({
         }`}
       >
         <Icon width={ICON_SIZE} height={ICON_SIZE} />
-        {update && (
-          <div
-            className={`absolute mb-2 ml-5 w-5 h-5 bg-primary-500 rounded-full flex justify-center items-center`}
-          >
+        {update > 0 && (
+          <div className="absolute mb-2 ml-5 w-5 h-5 bg-primary-500 rounded-full flex justify-center items-center">
             <p className="text-white text-xs">{update}</p>
           </div>
         )}
