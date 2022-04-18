ad4427729730107be9a6ab2005a44e4259282269
Itay Oshri <itay.oshri@gmail.com>
2022-04-18 16:16:48 +0300

    FEATURE: GroupOfChanges

---
+++ components/changes/GroupOfChanges.tsx
@@ -0,0 +1,39 @@
+import { ChangeInfo } from '../../hooks/useChanges'
+import useModification from '../../hooks/useModification'
+import { DayOfWeek, HourOfDay, LessonModification } from '../../interfaces'
+import { changeTextColor } from '../timetable/Lesson'
+import SingleChange from './SingleChange'
+
+interface ChangesGroupProps {
+  hour: HourOfDay
+  studyGroup?: string
+  changes: ChangeInfo[]
+}
+
+export default function GroupOfChanges({
+  hour,
+  studyGroup = '',
+  changes,
+}: ChangesGroupProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-[7px]">
+        {/* Hour */}
+        <p className="font-bold">
+          שעה {hour}
+          {!studyGroup && ':'}
+        </p>
+
+        {/* Study group*/}
+        {studyGroup && <p className="font-semibold">{studyGroup}:</p>}
+      </div>
+
+      {/* Changes*/}
+      <div className="flex flex-col pr-2">
+        {changes.map((change, index) => (
+          <SingleChange {...change} key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
