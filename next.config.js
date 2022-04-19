062d35a3a3b7e4261990e8a245a92aeaec8276a9
itayoshri <itay.oshri@gmail.com>
2022-04-19 14:34:18 +0300

    TEMP: Disable PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const withPWA = require('next-pwa')
+/*const withPWA = require('next-pwa')
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
+    fallbacks: {
+      image: '/static/images/fallback.png',
+      // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
+      // font: '/static/font/fallback.woff2',
+      // audio: ...,
+      // video: ...,
+    },
   },
 })
+*/
