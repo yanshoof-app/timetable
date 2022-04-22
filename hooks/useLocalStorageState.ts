f1668908f887a1f5370972661e0c4ff978623b7c
Itay Oshri <itay.oshri@gmail.com>
2022-04-23 01:11:38 +0300

    FIX: useLocalStorageState

--- hooks/useLocalStorageState.ts
+++ hooks/useLocalStorageState.ts
@@ -6,27 +6,6 @@ import {
   useState,
 } from 'react'
 
-const EXPIRES = 'expires'
-const COOKIE_LIFE = 60
-
-const valueFromCookie = (field: string, cookie: string) => {
-  return cookie.substring(field.length + 1)
-}
-
-const setCookie = (field, value) => {
-  const exprsDate = new Date()
-  exprsDate.setDate(exprsDate.getDate() + COOKIE_LIFE)
-  document.cookie = `${field}=${value};${EXPIRES}=${exprsDate}`
-}
-
-const getCookie = (field) => {
-  const cookies = document.cookie.split(';')
-  for (let cookie of cookies) {
-    if (cookie.substring(0, field.length) === field)
-      return valueFromCookie(field, cookie)
-  }
-}
-
 export interface ILocalStorageHandler<T> {
   decode(str?: string): T
   toStorable(value: T): string
@@ -39,17 +18,17 @@ export function createLocalStorageState<T>(
   { decode, toStorable }: ILocalStorageHandler<T>
 ) {
   return function useLocalStorageState(): [T, Dispatch<SetStateAction<T>>] {
-    const [isClientSide] = useState(typeof window != 'undefined') // loads quicker than the useClientSide() to prevent lugs
+    const [isClientSide] = useState(typeof localStorage != 'undefined') // loads quicker than the useClientSide() to prevent lugs
     const [value, setValue] = useState(
-      isClientSide ? decode(getCookie(field)) : undefined
+      isClientSide ? decode(localStorage.getItem(field)) : undefined
     )
 
     useEffect(() => {
-      if (isClientSide) setValue(decode(getCookie(field)))
+      if (isClientSide) setValue(decode(localStorage.getItem(field)))
     }, [isClientSide])
 
     const save = useCallback(() => {
-      isClientSide && setCookie(field, toStorable(value))
+      isClientSide && localStorage.setItem(field, toStorable(value))
     }, [value, isClientSide])
 
     useEffect(() => {
