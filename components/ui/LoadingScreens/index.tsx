932fc9da19c521e813eba1d156beb7b3ab55dcf7
Itay Oshri <itay.oshri@gmail.com>
2022-04-24 15:22:09 +0300

    DARKMODE: Loading screen

--- components/ui/LoadingScreens/index.tsx
+++ components/ui/LoadingScreens/index.tsx
@@ -15,7 +15,7 @@ export default function LoadingScreen({
       className={`flex justify-center flex-col items-center gap-3 ${className}`}
     >
       <Spinner className="w-16 h-16 fill-primary-500" />
-      <p className="font-semibold">{`${LOADING} ${label}...`}</p>
+      <p className="font-semibold dark:text-gray-300">{`${LOADING} ${label}...`}</p>
     </Layout>
   )
 }
