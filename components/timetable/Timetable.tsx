bb538e15e9f9934fe0ce8f4ee1b030303dff7948
itays123 <itays2005@gmail.com>
2022-05-03 13:34:10 +0300

    FIX: Unique key prop warning

--- components/timetable/Timetable.tsx
+++ components/timetable/Timetable.tsx
@@ -78,7 +78,7 @@ export default function Timetable({
                 key={`${day}${hour}`}
               />
             ) : (
-              <WindowSkeleton />
+              <WindowSkeleton key={`${day}${hour}`} />
             ))
         )}
     </div>
