1ed1da1f2145fd40062363baf38a074f28316483
itays123 <itays2005@gmail.com>
2022-05-03 13:51:01 +0300

    ERROR HANDLING: Searching lessons

--- components/ui/LoadingScreens/SearchingLessons.tsx
+++ components/ui/LoadingScreens/SearchingLessons.tsx
@@ -1,9 +1,10 @@
 import { ITeacherScheduleStatus } from '../../../hooks/useTeacherSchedule'
-import { Done } from '../../icons'
+import { Done, Warning } from '../../icons'
 import Spinner from './Spinner'
 
 const LOADING_LESSONS = 'מחפש שיעורים'
 const LESSONS_FOUND = 'שיעורים נמצאו'
+const SEARCH_INTERRUPTED = 'חיפוש לא הושלם, נסו שנית מאוחר יותר'
 
 function toPercent(number) {
   return Math.round(number * 100)
@@ -14,26 +15,34 @@ export default function SearchingLessons({
   classesSearched,
   totalClasses,
   isLoading,
+  error,
 }: ITeacherScheduleStatus) {
   return (
-    <div className="flex px-4 items-center gap-2">
+    <div
+      className={`flex px-4 gap-2 ${error ? 'items-start' : ' items-center'}`}
+    >
       {isLoading ? (
         <Spinner className="h-4 w-4 fill-primary-500" />
+      ) : error ? (
+        <Warning width={24} height={24} className="fill-primary-500 mt-1" />
       ) : (
         <Done width={24} height={24} className="fill-primary-500" />
       )}
-      <p className="dark:text-gray-300 font-semibold text-lg">
-        {isLoading ? (
-          <>
-            <span>{LOADING_LESSONS}</span>
-            <span> ({toPercent(classesSearched / totalClasses)}%)</span>
-          </>
-        ) : (
-          <span>
-            {lessonsFound} {LESSONS_FOUND}
-          </span>
-        )}
-      </p>
+      <div className="dark:text-gray-300">
+        <p className="font-semibold text-lg">
+          {isLoading ? (
+            <>
+              <span>{LOADING_LESSONS}</span>
+              <span> ({toPercent(classesSearched / totalClasses)}%)</span>
+            </>
+          ) : (
+            <span>
+              {lessonsFound} {LESSONS_FOUND}
+            </span>
+          )}
+        </p>
+        {error && <span>{SEARCH_INTERRUPTED}</span>}
+      </div>
     </div>
   )
 }
