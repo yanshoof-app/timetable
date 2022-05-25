5b552d1d597374aefe883c688e3e6b157bdee8e0
itays123 <itays2005@gmail.com>
2022-05-25 15:50:05 +0300

    FIX: DayPick more efficient

--- components/forms/DayPick.tsx
+++ components/forms/DayPick.tsx
@@ -18,32 +18,24 @@ export default function DayPick({
   onChange = () => {},
   dayFilterer = () => true,
 }: DayPickProps) {
-  const [value, setValue] = useState(day)
-
-  useEffect(() => {
-    onChange(value)
-  }, [value, onChange])
-
-  const filteredDays = useMemo(
-    () => HEBREW_SHORT_DAYS.filter(dayFilterer),
-    [dayFilterer]
-  )
-
   return (
     <div className={`flex w-[full] justify-between items-center ${className}`}>
-      {filteredDays.map((day, index) => (
-        <Button
-          className={`${
-            value !== index && 'text-black dark:text-gray-300 bg-transparent'
-          } font-semibold`}
-          onClick={() => {
-            setValue(index as DayOfWeek)
-          }}
-          key={index}
-        >
-          {day}
-        </Button>
-      ))}
+      {HEBREW_SHORT_DAYS.map(
+        (currentDay, index) =>
+          dayFilterer(currentDay, index) && (
+            <Button
+              className={`${
+                day !== index && 'text-black dark:text-gray-300 bg-transparent'
+              } font-semibold`}
+              onClick={() => {
+                onChange(index as DayOfWeek)
+              }}
+              key={index}
+            >
+              {currentDay}
+            </Button>
+          )
+      )}
     </div>
   )
 }
