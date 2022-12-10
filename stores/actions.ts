1e0c20d4ca60913e40f27bb4b400406cf1086032
Itay Schechner <itays2005@gmail.com>
2022-12-10 14:24:27 +0200

    FEATURE: Set class options, set class

--- stores/actions.ts
+++ stores/actions.ts
@@ -4,7 +4,7 @@ import { resetSettings } from "./state/settings";
 import { resetTimetableState } from "./state/timetable";
 
 export const setSchool = (symbol: string | number, name: string) => {
-    if (state.school.schoolSymbol != symbol) {
+    if (state.school.schoolSymbol !== symbol) {
         state.school.schoolSymbol = symbol;
         state.school.schoolName = name;
         resetClassState();
@@ -13,3 +13,20 @@ export const setSchool = (symbol: string | number, name: string) => {
     }
 }
 
+export const setClassOptions = (gradeOptions: number[], classIdOptions: number[][]) => {
+    if (gradeOptions.length > state.class.gradeOptions.length
+        && classIdOptions.length == gradeOptions.length) {
+        state.class.gradeOptions = gradeOptions;
+        state.class.classIdOptions = classIdOptions;
+    }
+}
+
+export const setClass = (classId: string, grade: number, classNum: number) => {
+    if (state.class.classId !== classId) {
+        state.class.classId = classId;
+        state.class.grade = grade;
+        state.class.classNum = classNum;
+        resetSettings();
+        resetTimetableState();
+    }
+} 
