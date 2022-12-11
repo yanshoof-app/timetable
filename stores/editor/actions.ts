8cba687d0b1a24f610bcf0791dec3598f46394e3
Itay Schechner <itays2005@gmail.com>
2022-12-11 21:06:11 +0200

    FEATURE: Export settings

--- stores/editor/actions.ts
+++ stores/editor/actions.ts
@@ -3,8 +3,12 @@ import { editorState } from "."
 import { state } from "../persisted";
 import { IPersistedSettings } from "../persisted/state/settings";
 
-export const toDayHourString = (day: DayOfWeek, hour: HourOfDay) => {
+const toDayHourString = (day: DayOfWeek, hour: HourOfDay) => {
     return `${day}${hour}`
+}
+
+const fromDayHourString = (dayHourString: string): [DayOfWeek, HourOfDay] => {
+    return dayHourString.split(",").map(Number) as [DayOfWeek, HourOfDay]
 }
 
 export const importStoredSettings = () => {
@@ -59,17 +63,44 @@ export const setWindow = (day: DayOfWeek, hour: HourOfDay) => {
 
 export const removeProblems = () => {
     editorState.fullTimetable.problems.forEach((dayHourString) => {
-        const [day, hour] = dayHourString.split(",");
-        setWindow(day as unknown as DayOfWeek, hour as unknown as HourOfDay);
+        const [day, hour] = fromDayHourString(dayHourString);
+        setWindow(day, hour);
     })
 }
 
 export const exportSettings = () => {
     // remove schedule settings of was-windows
+    /*
+    * Description of problem to solve here:
+    * When the user manually sets an hour with one lesson option to WINDOW (day,hour=>-1)
+    * And then RESETS the hour to the option, this will create a situation where there might be
+    * an undetectable unused study group.
+    * Method of tackling:
+    * - Go through each hour overriden by settings
+    * - If the setting there is not -1, and the hour has 1 option, remove the setting 
+    */
+    let studyGroupMap = [...editorState.settings.studyGroupMap].filter(([dayHourString, sgIndex]) => {
+        const [day, hour] = fromDayHourString(dayHourString);
+        return sgIndex == -1 || editorState.fullTimetable.lessons[day][hour].length > 1
+    })
+
     // remove unused study groups
-    return {
-        studyGroups: [...editorState.settings.studyGroups],
-        studyGroupMap: [...editorState.settings.studyGroupMap.entries()]
-    } as IPersistedSettings;
+    /*
+    * Description of problem to tackle here:
+    * Often times there are unused study groups in the study group array. We would like to remove them.
+    * Method of tackling:
+    * - Create a set of indexes 0..(length-1)
+    * - For each mapping, remove the mapping index from the set
+    * - The remaining indexes in the set are unused
+    */
+    let studyGroups = [...editorState.settings.studyGroups]
+    const unusedIndexes = new Set([...Array(editorState.settings.studyGroups.length).keys()]);
+    studyGroupMap.forEach(([_, index]) => unusedIndexes.delete(index));
+    unusedIndexes.forEach((unusedIndex) => {
+        studyGroups = studyGroups.splice(unusedIndex, 1);
+        studyGroupMap = studyGroupMap.map(([dayHour, index]) => ([dayHour, index > unusedIndex ? index - 1 : index]));
+    })
+
+    return { studyGroups, studyGroupMap } as IPersistedSettings;
 }
 
